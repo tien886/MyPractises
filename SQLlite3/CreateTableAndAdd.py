66e1b27c138242e941c283096f26461c89637952
TIEN <tienvm886@gmail.com>
2025-07-24 14:50:25 +0700

    Create and Insert

---
+++ SQLlite3/CreateTableAndAdd.py
@@ -0,0 +1,57 @@
+import sqlite3
+conn = sqlite3.connect('car.db')
+c = conn.cursor()
+sql_create_table = """
+    CREATE TABLE cars (
+        name text,
+        brand text,
+        year integer
+    )
+"""
+# sql_insert_table = f"""
+#     INSERT INTO cars VALUES(
+#     '{}', 
+#     '{}',
+#     {}
+#     )
+# """
+def Insert():
+    print("Nhập tên xe:")
+    name = input()
+
+    print("Nhập hãng xe:")
+    brand = input()
+
+    print("Nhập năm sản xuất:")
+    year = int(input())
+    sql_insert_table = f"""
+    INSERT INTO cars VALUES(
+    '{name}',
+    '{brand}',
+    {year}
+    )
+    """
+    return sql_insert_table
+def Print():
+    conn = sqlite3.connect('car.db')
+    c = conn.cursor()
+    sql_selectAll_table = """
+        SELECT * FROM cars
+    """
+    c.execute(sql_selectAll_table)
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+def main():
+    try:
+        # c.execute(sql_create_table)s
+        while(True):
+            option = input()
+            if(option == "0"): break
+            conn.execute(Insert())
+        conn.commit()
+    except Exception as e:
+        print("Error:", e)
+    conn.close()
+    Print()
+main()
