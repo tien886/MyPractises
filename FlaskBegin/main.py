9d8ac199a752e8cc79969f1b9e6c3dd333136574
TIEN <tienvm886@gmail.com>
2025-07-24 13:38:50 +0700

    Modify the UI and Correct Flash message mechanism

--- FlaskBegin/main.py
+++ FlaskBegin/main.py
@@ -10,8 +10,9 @@ def home():
     return render_template('home.html')
 @app.route('/user')
 def hello_user():
-    name = session.get("user")
-    if name:
+    name = session.get("user")  #get name value from 'user' index in hashtable (session)
+    if name:    #name not NULL
+        flash("You logged in successfully", "info")
         return render_template('user.html', user = name)
     return redirect(url_for('login'))
 @app.route('/admin')
@@ -21,20 +22,24 @@ def hello_admin():
 @app.route('/login', methods = ["POST", "GET"])
 def login():
     if request.method == "POST":
-        user_name = request.form["full_name"]
-        session.permanent = True
-        flash("You logged in succesfully","info")
-        print(user_name)
+        user_name = request.form["full_name"]   #full_name is defined in HTML file
+        pass_word = request.form["pass_word"]   #pass_word is defined in HTML file
+        if not user_name or not pass_word:
+            flash("Login Failed! Try again", "error")
+            return render_template('login.html')
+        session.permanent = True    # this is lifetime for user to visit a website 
         if user_name in admin:    
+            flash("You logged in successfully as admin", "info")
             return redirect(url_for("hello_admin"))
-        elif user_name:
-            session["user"] = user_name
-            return redirect(url_for("hello_user"))
+        session["user"] = user_name
+        session["pass"] = pass_word
+        print(session)
+        return redirect(url_for("hello_user"))
     return render_template('login.html')
 @app.route('/logout')
 def logout():
     session.pop("user", None)
-    flash("You logged out succesfully","info")
+    flash("You logged out succesfsully","info")
     return redirect(url_for("login"))
 if __name__ == "__main__":
     app.run(debug = True)
