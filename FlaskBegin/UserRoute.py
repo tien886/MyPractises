62c166cc29c82bb20b047425e374b2ec72ac1a8f
TIEN <tienvm886@gmail.com>
2025-07-26 15:01:49 +0700

    Create a route for user

---
+++ FlaskBegin/UserRoute.py
@@ -0,0 +1,14 @@
+from flask import Flask, redirect, url_for, render_template, request,session, flash, Blueprint
+user = Blueprint("user", __name__)
+
+@user.route('/user')
+def user_page():
+    name = session.get("user")  #get name value from 'user' index in hashtable (session)
+    if name:    #name not NULL
+        return render_template('user.html', user = name)
+    return redirect(url_for('login_page'))
+@user.route('/logout')   
+def log_out():
+    session.pop("user", None)
+    flash("You logged out successfully","info")
+    return redirect(url_for("login_page"))
--- FlaskBegin/main.py
+++ FlaskBegin/main.py
@@ -1,45 +1,64 @@
 from flask import Flask, redirect, url_for, render_template, request,session, flash
+from flask_sqlalchemy import SQLAlchemy
 from datetime import timedelta
-
+from os import path
+from purify import Purify
+from extensions import db
+from user import User
+from UserRoute import user
+from AdminRoute import admin
+from restriction import Restriction
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "abcd1234"
-app.permanent_session_lifetime = timedelta(seconds=30)
+app.permanent_session_lifetime = timedelta(minutes=30)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+db.init_app(app)
+app.register_blueprint(user)
+app.register_blueprint(admin)
+
 admin = ["TIEN", "VOMINHTIEN", "MINHTIEN"]
 @app.route('/')
 def home():
     return render_template('home.html')
-@app.route('/user')
-def hello_user():
-    name = session.get("user")  #get name value from 'user' index in hashtable (session)
-    if name:    #name not NULL
-        flash("You logged in successfully", "info")
-        return render_template('user.html', user = name)
-    return redirect(url_for('login'))
-@app.route('/admin')
-def hello_admin():
-    print("OK")
-    return render_template('xinchaoadmin.html')
+
 @app.route('/login', methods = ["POST", "GET"])
-def login():
+def login_page():
     if request.method == "POST":
         user_name = request.form["full_name"]   #full_name is defined in HTML file
         pass_word = request.form["pass_word"]   #pass_word is defined in HTML file
-        if not user_name or not pass_word:
-            flash("Login Failed! Try again", "error")
-            return render_template('login.html')
-        session.permanent = True    # this is lifetime for user to visit a website 
+        print(pass_word)
+        # print(user_name)
         if user_name in admin:    
-            flash("You logged in successfully as admin", "info")
-            return redirect(url_for("hello_admin"))
-        session["user"] = user_name
-        session["pass"] = pass_word
-        print(session)
-        return redirect(url_for("hello_user"))
+            flash("YOU LOGGED IN SUCCESSFULLY AS ADMIN", "info")
+            return redirect(url_for("admin.admin_page"))
+        if not Restriction.CheckPassWordInFormat(pass_word):
+            flash("YOUR PASSWORD IS WRONG IN FORMAT! TRY AGAIN", "error")
+            return redirect(url_for('login_page'))
+        if not user_name or not pass_word:
+            flash("LOGIN FAILED! TRY AGAIN", "error")
+            return redirect(url_for('login_page'))
+        session.permanent = True    # this is lifetime for user to visit a website 
+        user = User.query.filter_by(name=user_name).first()
+        if user:
+            if user.password == Purify.HashPassWord(pass_word):
+                print(Purify.HashPassWord(pass_word))
+                flash("LOGIN SUCCESSFULLY!", "info")
+                session["user"] = user_name
+                return redirect(url_for("user.user_page"))
+            else:
+                flash("WRONG PASSWORD!", "error")
+                return redirect(url_for('login_page'))
+        return redirect(url_for("user.user_page"))
     return render_template('login.html')
-@app.route('/logout')
-def logout():
-    session.pop("user", None)
-    flash("You logged out succesfsully","info")
-    return redirect(url_for("login"))
+
 if __name__ == "__main__":
-    app.run(debug = True)
+    try:
+        with app.app_context():
+            if not path.exists("user.db"):
+                if not path.exists("user.db"):
+                    db.create_all()
+                    print("Created table Successfully")
+        app.run(debug = True)
+    except Exception as e:
+        print("Error: ", e)
